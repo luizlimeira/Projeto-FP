c8248d5cf32f78b727b47bbe81deb06f034fa714
Luiz Limeira <139459919+luizlimeira@users.noreply.github.com>
2025-05-21 15:15:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+from cardapio import *
+from ficha import *
+
+def menu_fichas():
+    while True:
+        fichas = carregar_fichas()
+        
+        print("\n--- MENU FICHAS ---")
+        print("1. Adicionar Ficha")
+        print("2. Visualizar Fichas")
+        print("3. Editar Ficha")
+        print("4. Excluir Ficha")
+        print("0. Voltar")
+        
+        opcao = input("Escolha uma opção: ")
+        
+        if opcao == "1":
+            fichas = adicionar_ficha(fichas)
+        elif opcao == "2":
+            visualizar_fichas(fichas)
+        elif opcao == "3":
+            editar_fichas(fichas)
+        elif opcao == "4":
+            excluir_fichas(fichas)
+        elif opcao == "0":
+            return
+        else:
+            print("\nOpção inválida!")
+
 def menu_cardapio():
     while True:
         print("\n--- CARDÁPIO ---")
@@ -5,8 +34,24 @@ def menu_cardapio():
         print("2. Adicionar")
         print("3. Atualizar")
         print("4. Remover")
+        print("5. Filtrar")
         print("0. Voltar")
         op = input("Escolha: ")
+
+        if op == "1":
+            listar_cardapio()
+        elif op == "2":
+            adicionar_item()
+        elif op == "3":
+            atualizar_item()
+        elif op == "4":
+            remover_item()
+        elif op == "5":
+            filtrar_por_categoria()
+        elif op == "0":
+            break    
+        else:
+            print("Opção inválida.")
 
 def main():
     while True:
@@ -34,4 +79,5 @@ def main():
             print("Opção inválida.")
 
 if __name__ == "__main__":
+    main()
     main()
